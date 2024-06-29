2d52cd761c216aa4fb875d3b2f4a141c0432d817
Jd programmer <johndennycastano@gmail.com>
2024-06-29 16:42:25 -0500

    construccion del modelo

--- tiendamaximo/producto/models.py
+++ tiendamaximo/producto/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    codigo = models.CharField(max_length=1000)
+    articulo=models.CharField(max_length=1000)
+    ingreso=models.IntegerField()
+    salida=models.IntegerField()
+    stock= models.IntegerField()
+
+class Entrada(models.Model):
+    fechaEntrada = models.DateTimeField() 
+    cantidadComprada = models.IntegerField() 
+    
+c    
+    
+
